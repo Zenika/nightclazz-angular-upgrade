a28f60b8467550e227e7fd47e1f7a059e1e0ff20
Arthur DREVOT <arthur@drevot.fr>
2023-02-21 20:52:45 +0100

    📃 Use typed forms when create agency

--- src/app/modules/create/create.component.ts
+++ src/app/modules/create/create.component.ts
@@ -1,8 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {UntypedFormBuilder, UntypedFormGroup, Validators} from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import {CitiesService} from "../../shared/services/cities.service";
 import {Router} from "@angular/router";
 import { GeoPosition } from "../../core/domain/geo-position";
+
+interface CreateForm {
+  name: FormControl<string>
+  latitude: FormControl<number>
+  longitude: FormControl<number>
+}
 
 @Component({
   selector: 'app-create',
@@ -10,21 +16,21 @@ import { GeoPosition } from "../../core/domain/geo-position";
   styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
-  public form: UntypedFormGroup;
+  public form!: FormGroup<CreateForm>;
 
-  constructor(protected fb: UntypedFormBuilder, protected citiesService: CitiesService, protected router: Router) {
+  constructor(protected fb: FormBuilder, protected citiesService: CitiesService, protected router: Router) {
   }
 
   ngOnInit(): void {
-    this.form = this.fb.group({
+    this.form = this.fb.nonNullable.group({
       name: ['', Validators.required],
-      latitude: ['', [Validators.required, Validators.pattern(/^\d+.?\d*$/), Validators.max(180), Validators.min(-180)]],
-      longitude: ['', [Validators.required, Validators.pattern(/^\d+.?\d*$/), Validators.max(180), Validators.min(-180)]],
+      latitude: [0, [Validators.required, Validators.pattern(/^\d+.?\d*$/), Validators.max(180), Validators.min(-180)]],
+      longitude: [0, [Validators.required, Validators.pattern(/^\d+.?\d*$/), Validators.max(180), Validators.min(-180)]],
     })
   }
 
   createCity() {
-    const cityToAdd = this.form.value;
+    const cityToAdd = this.form.getRawValue();
     this.citiesService.addCity({name: cityToAdd.name, position: new GeoPosition(cityToAdd.latitude, cityToAdd.longitude)});
     this.router.navigate(['/']);
   }
